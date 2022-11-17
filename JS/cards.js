4f251672fbe04a50746da8d6b08b3402d5674b7c
moria2115 <moria2115@gmail.com>
2022-11-17 20:53:46 +0200

    card js + deletion

--- JS/cards.js
+++ JS/cards.js
@@ -1,7 +1,51 @@
 let sponsers = [
     {
-        title:
-            image:
-        link:
-    }
+        title: 'Ace Hotel',
+        image: '../images/sponsers/aceHotel.png',
+        link: "https://acehotel.com/new-orleans/",
+    },
+    {
+        title: 'Haunted Museum',
+        image: '../images/sponsers/hauntedMuseum.jpg',
+        link: "https://thehauntedmuseum.com/",
+    },
+    {
+        title: "Louisiana Children Museum",
+        image: '../images/sponsers/louisianaChildrenMuseum.png',
+        link: "https://lcm.org/",
+    },
+    {
+        title: 'Museum of Death',
+        image: '../images/sponsers/museumOfDeath.png',
+        link: "http://www.museumofdeath.net/",
+    },
+    {
+        title: "Premium Parking",
+        image: '../images/sponsers/premiumParking.png',
+        link: "https://www.premiumparking.com/",
+    },
+    {
+        title: 'Sheraton',
+        image: '../images/sponsers/sheraton.png',
+        link: "https://www.marriott.com/en-us/hotels/msyis-sheraton-new-orleans-hotel/overview/",
+    },
+    {
+        title: 'The Ritz-Carlton',
+        image: '../images/sponsers/theRitzCarlton.png',
+        link: "https://www.ritzcarlton.com/en/hotels/new-orleans",
+    },
+    {
+        title: "Walk On's",
+        image: '../images/sponsers/walkOns.png',
+        link: "https://walk-ons.com/",
+    },
 ]
+
+function sponsersCard(params) {
+
+}
+
+
+
+
+
